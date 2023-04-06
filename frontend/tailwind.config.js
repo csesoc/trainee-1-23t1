d2d9076b0ac8a1125e564f11e81b8ad64fec6fca
CHUN Nicole Wai Yan <19chunnw3@gmail.com>
2023-04-06 15:29:58 +1000

    added colours :D

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      yellow: "#EEBE6F",
+      pink: "#F28482",
+      blue: "#8BA49D",
+      black: "#222222",
+      white: "#F5EDE3",
+      Pink: "#EECCC5",
+    },
     extend: {},
   },
   plugins: [],
