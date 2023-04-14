26a7fdb55b76736b1e28f92784de6e9e291ca96b
Chloe Wong <chloe.wong@student.unsw.edu.au>
2023-04-14 14:09:14 +1000

    implemented navigation to register page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,11 @@
-// import { useNavigate } from 'react-router';
+import { useNavigate } from 'react-router-dom';
 
 function Login () {
-    // const navigate = useNavigate();
+  const navigate = useNavigate();
+  const navToRegister = () => {
+    navigate('/admin/auth/register');
+  }
+
   return (
     <div className="container flex justify-center items-center min-h-screen bg-theme-yellow">
       <div className="container flex-auto max-w-md max-h-min px-10 py-5 rounded-xl shadow-md bg-theme-white">
@@ -10,10 +14,10 @@ function Login () {
         <form>
           <div className="container flex items-start flex-col">
             <p className="text-sm">Email</p>
-            <input type="email" className="form-input w-full px-3 py-2 mt-2 rounded-xl border-0" placeholder="Enter your email"></input>
+            <input type="email" className="form-input w-full px-3 py-2 mt-2 rounded-xl border-0 text-sm" placeholder="Enter your email"></input>
             <br />
             <p className="text-sm">Password</p>
-            <input type="password" className="form-input w-full px-3 py-2 mt-2 rounded-xl border-0" placeholder="Enter your password"></input>
+            <input type="password" className="form-input w-full px-3 py-2 mt-2 rounded-xl border-0 text-sm" placeholder="Enter your password"></input>
             <br />
             <button className="w-full px-2 py-3 rounded-xl border-0 bg-theme-red hover:bg-[#e37876]">
               <p className="font-bold">Login</p>
@@ -23,7 +27,7 @@ function Login () {
         <br />
         <div className="flex flex-col justify-center">
           <p className="font-bold text-sm mb-2">Not a member?</p>
-          <p className="font-bold text-sm text-theme-blue">Register</p>
+          <button className="font-bold text-sm text-theme-blue hover:underline" onClick={navToRegister}>Register</button>
         </div>
       </div>
     </div>
