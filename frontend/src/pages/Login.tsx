24144e2739a113f9a1003797646f5af799edbab3
Chloe Wong <chloe.wong@student.unsw.edu.au>
2023-04-14 13:53:13 +1000

    completed login page

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,34 @@
+// import { useNavigate } from 'react-router';
+
+function Login () {
+    // const navigate = useNavigate();
+  return (
+    <div className="container flex justify-center items-center min-h-screen bg-theme-yellow">
+      <div className="container flex-auto max-w-md max-h-min px-10 py-5 rounded-xl shadow-md bg-theme-white">
+        <p className="font-bold text-3xl">Login</p>
+        <br />
+        <form>
+          <div className="container flex items-start flex-col">
+            <p className="text-sm">Email</p>
+            <input type="email" className="form-input w-full px-3 py-2 mt-2 rounded-xl border-0" placeholder="Enter your email"></input>
+            <br />
+            <p className="text-sm">Password</p>
+            <input type="password" className="form-input w-full px-3 py-2 mt-2 rounded-xl border-0" placeholder="Enter your password"></input>
+            <br />
+            <button className="w-full px-2 py-3 rounded-xl border-0 bg-theme-red hover:bg-[#e37876]">
+              <p className="font-bold">Login</p>
+            </button> 
+          </div>
+        </form>
+        <br />
+        <div className="flex flex-col justify-center">
+          <p className="font-bold text-sm mb-2">Not a member?</p>
+          <p className="font-bold text-sm text-theme-blue">Register</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login;
+
