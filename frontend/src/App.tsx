09e21a3abe40c9bd17b50476b9fe5a1c8493d42c
Chloe Wong <chloe.wong@student.unsw.edu.au>
2023-04-14 14:08:46 +1000

    Added route to register

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/admin/auth/login' element={<Login/>} />
+        <Route path='/admin/auth/register' element={<Register/>} />
       </Routes>
     </BrowserRouter>
   );
