5efb5d4a88105e11e1661b09c0c5a0e09cf6ef0a
Chloe Wong <chloe.wong@student.unsw.edu.au>
2023-04-14 14:48:29 +1000

    Added route for details

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Details from './pages/Details';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/admin/auth/login' element={<Login/>} />
         <Route path='/admin/auth/register' element={<Register/>} />
+        <Route path='/admin/auth/details' element={<Details/>} />
       </Routes>
     </BrowserRouter>
   );
