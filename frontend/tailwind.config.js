7cf84f20f7d9c707186cbab1b0a60f4c3cce75da
Chloe Wong <chloe.wong@student.unsw.edu.au>
2023-04-14 13:52:19 +1000

    added theme colours

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'theme-yellow': '#EEBE6F',
+        'theme-red': '#F28482',
+        'theme-pink': '#EECCC5',
+        'theme-blue': '#84A59D',
+        'theme-white': '#F7EDE2'
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
 };
