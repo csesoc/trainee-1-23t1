33ae338e069f49e8b61c6951b980af37e31db0b1
nicole <107306795+crocyoiin@users.noreply.github.com>
2023-05-28 18:47:40 +1000

    forgot to remove some stuff

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -2,17 +2,6 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-export const firebaseConfig2 = {
-  apiKey: "AIzaSyAol-3f0sIO9ELIEz1dMsSHMJAEv0JZ2Uo",
-  authDomain: "trainee-1-23t1.firebaseapp.com",
-  databaseURL: "https://trainee-1-23t1-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "trainee-1-23t1",
-  storageBucket: "trainee-1-23t1.appspot.com",
-  messagingSenderId: "135738959429",
-  appId: "1:135738959429:web:fdd9285783e2672f0e652c",
-  measurementId: "G-XSKELRHSNN"
-};
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   projectId: import.meta.env.VITE_PROJECT_ID,
@@ -20,7 +9,7 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig2);
+const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
