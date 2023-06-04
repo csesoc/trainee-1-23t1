c107751ad99a4729e7c909daf07f5650d95b3eca
Chloe Wong <chloe.wong@student.unsw.edu.au>
2023-06-04 12:31:03 +1000

    bug fixed

--- frontend/src/components/PartnrDisplay.tsx
+++ frontend/src/components/PartnrDisplay.tsx
@@ -35,25 +35,28 @@ function PartnrDisplay({partnrInfo}: Props) {
    }
 
    return (
-      <div className='flex relative bg-theme-yellow items-center justify-center col-span-1 p-10 rounded-2xl shadow-md hover:shadow-black'>
+      <div className='flex relative bg-theme-yellow items-center justify-center col-span-1 p-6 rounded-2xl shadow-md hover:shadow-md-black'>
          <div className='flex basis-1/3 cursor-pointer' onClick={navToPartnrPage}>
-            <img className='p-3' src={partnrInfo.photo}/>
+            <img className='rounded-full' src={partnrInfo.photo}/>
          </div>
-         <div className='flex flex-col basis-2/3 cursor-pointer p-3'  onClick={navToPartnrPage}>
+         <div className='flex flex-col basis-2/3 cursor-pointer p-3 text-center'  onClick={navToPartnrPage}>
             <b>{partnrInfo.name}</b>
             <p className='text-sm'>{yearToString(partnrInfo.year)}</p>
             <p className='text-sm'>{partnrInfo.degree}</p>
-            <div className='flex justify-between'>
-               <p className='text-sm'><b>Desired Grade </b></p>
-               <p className='text-sm'>HD</p>
-            </div>
-            <div className='flex justify-between'>
-               <p className='text-sm'><b>Hours per week </b></p>
-               {/* <p className='text-sm'>{partnrInfo.hours}</p> */}
-            </div>
-            <div className='flex justify-between'>
-               <p className='text-sm'><b>Preferred Comms </b></p>
-               <p className='text-sm'>{partnrInfo.communication}</p>
+            <br/>
+            <div className='flex flex-row'>
+               <div className='flex flex-col justify-between text-center'>
+                  <p className='text-sm'><b>Desired Grade </b></p>
+                  <p className='text-sm'>HD</p>
+               </div>
+               <div className='flex flex-col justify-between text-center'>
+                  <p className='text-sm'><b>Hours / week </b></p>
+                  {/* <p className='text-sm'>{partnrInfo.hours}</p> */}
+               </div>
+               <div className='flex flex-col justify-between text-center'>
+                  <p className='text-sm'><b>Preferred Comms </b></p>
+                  <p className='text-sm'>{partnrInfo.communication}</p>
+               </div>
             </div>
          </div>
       </div>
--- frontend/src/pages/ListPartnrs.tsx
+++ frontend/src/pages/ListPartnrs.tsx
@@ -4,36 +4,57 @@ import PartnrDisplay from '../components/PartnrDisplay';
 import { db } from '../firebase';
 import { doc, getDoc } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
+import { useAuth } from '../context/AuthContext';
 
 function ListPartnrs() {
    const [users, setUsers] = useState<any[]>([]);
+   const [curZid, setCurZid] = useState('');
    const [loading, setLoading] = useState(true);
    const { courseId } = useParams();
 
-   useEffect(() => {
-      const getUsers = async () => {
-         const userList: any[] = [];
-         if (courseId) {
-            const courseRef = doc(db, "courses", courseId);
-            const courseSnap = await getDoc(courseRef);
-            const userIds = courseSnap.data()?.users;
-            userIds.forEach(async (id: string) => {
-               const userRef = doc(db, "users", id);
-               const userSnap = await getDoc(userRef);
-               const userData = userSnap.data();
-               userList.push(userData);
-            });
-         }
-         setUsers(userList);
+   const { authUid } = useAuth();
+
+   useEffect(() =>{
+      setTimeout(() => {
          setLoading(false);
-      }
+       }, 1000);
+   });
+
+   useEffect(() => {
       getUsers();
-   }, [])
+   }, []);
    
-      
+   const getUsers = async () => {
+      const userList: any[] = [];
+      let curZid = 0;
+      if (authUid) {
+         const userRef = doc(db, "users", authUid);
+         const userSnap = await getDoc(userRef);
+         const userData = userSnap.data();
+         if (userData) {
+            curZid = userData.zid;
+         }
+      }
+      if (courseId) {
+         const courseRef = doc(db, "courses", courseId);
+         const courseSnap = await getDoc(courseRef);
+         const userIds = courseSnap.data()?.users;
+         userIds.forEach(async (id: any) => {
+            const userRef = doc(db, "users", id);
+            const userSnap = await getDoc(userRef);
+            const userData = userSnap.data();
+            if (userData && userData.zid !== curZid) {
+               userList.push(userData);
+            }
+         });
+      }
+      setUsers(userList);
+   }
+   
+   
+   console.log(users);
    const listPartnrs =
-      users.map((user) => {
-         console.log(user.zid);
+   users.map((user) => {
          const partnrInfo = {
             zid: user.zid,
             name: user.name,
@@ -44,7 +65,7 @@ function ListPartnrs() {
             //hours: number
             communication: user.comms,
          };
-         return <PartnrDisplay key={user.zid} partnrInfo={partnrInfo} />;
+         return <PartnrDisplay key={user.zid} partnrInfo={partnrInfo} />
       });
 
    return (
